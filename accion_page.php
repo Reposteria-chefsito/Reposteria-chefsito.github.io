6738e9c59d7bf38e7a51dd58f2dfceba23d44d66
KarlaCastroOlalde <99769850+KarlaCastroOlalde@users.noreply.github.com>
2022-03-22 16:53:55 -0600

    Update accion_page.php

--- accion_page.php
+++ accion_page.php
@@ -2,7 +2,17 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
+Your name address is: <?php echo $_POST["nombre"]; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your apellidos address is: <?php echo $_POST["apellidos"]; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your contacto address is: <?php echo $_POST["contacto"]; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your tipo de pastel address is: <?php echo $_POST["tipo de pastel"]; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your fecha de entrega address is: <?php echo $_POST["fecha de entrega"]; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your hora de entrega address is: <?php echo $_POST["hora de entrega"]; ?>
+  
 </body>
 </html>
