6074304737fccd128a64ab2b2d3cdc1b3cc98f50
KarlaCastroOlalde <99769850+KarlaCastroOlalde@users.noreply.github.com>
2022-03-22 16:58:04 -0600

    Update accion_page.php

--- accion_page.php
+++ accion_page.php
@@ -1,18 +1,18 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your name address is: <?php echo $_POST["nombre"]; ?>
-Welcome <?php echo $_POST["name"]; ?><br>
-Your apellidos address is: <?php echo $_POST["apellidos"]; ?>
-Welcome <?php echo $_POST["name"]; ?><br>
-Your contacto address is: <?php echo $_POST["contacto"]; ?>
-Welcome <?php echo $_POST["name"]; ?><br>
-Your tipo de pastel address is: <?php echo $_POST["tipo de pastel"]; ?>
-Welcome <?php echo $_POST["name"]; ?><br>
-Your fecha de entrega address is: <?php echo $_POST["fecha de entrega"]; ?>
-Welcome <?php echo $_POST["name"]; ?><br>
-Your hora de entrega address is: <?php echo $_POST["hora de entrega"]; ?>
+<?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["nombre"]; ?>
+<?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["apellidos"]; ?>
+<?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["contacto"]; ?>
+<?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["tipo de pastel"]; ?>
+<?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["fecha de entrega"]; ?>
+<?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["hora de entrega"]; ?>
   
 </body>
 </html>
